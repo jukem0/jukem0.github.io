53451bd99e8f5a1e4a68fbd393e16febbd05e011
jukem0 <93048109+jukem0@users.noreply.github.com>
2022-01-23 21:24:10 +0100

    Add files via upload

---
+++ close.js
@@ -0,0 +1,31 @@
+
+
+function closeInf() {
+    var x = document.getElementById("infGf");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function closeCodeHTML() {
+    var x = document.getElementById("codeHTML");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function closeProg() {
+    var x = document.getElementById("prog");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+

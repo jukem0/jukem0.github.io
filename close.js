a71f645fe33c20c0299c282040440974a973c12e
jukem0 <93048109+jukem0@users.noreply.github.com>
2022-01-28 10:39:01 +0100

    Add files via upload

--- close.js
+++ close.js
@@ -11,19 +11,19 @@ function closeInf() {
 
 function closeCodeHTML() {
     var x = document.getElementById("codeHTML");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
         x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
 }
 
 function closeProg() {
     var x = document.getElementById("prog");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
         x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
 }
 
